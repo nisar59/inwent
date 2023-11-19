e08361cb52974095c5336a7dd7bd11860750b077
Muhammed Yaqub <hiiamyaqub268@gmail.com>
2023-11-19 23:14:18 +0500

    comit

--- CMS/OurClient/Entities/OURClient.php
+++ CMS/OurClient/Entities/OURClient.php
@@ -8,15 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class OurClient extends Model
 {
     use HasFactory;
-
-<<<<<<< HEAD
     protected $fillable = ['image'];
     protected $table='our_clients';
-=======
-   	protected $fillable = ['image'];
-    protected $table='our_client';
->>>>>>> a641e6da61707e6692674ec7ee1a6a7e399f0426
-    
     
     protected static function newFactory()
     {
