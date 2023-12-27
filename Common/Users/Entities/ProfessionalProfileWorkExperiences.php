20b36b308bbd32236ff6e678f6582c6df7a4e1e8
Mahar Nisar <umair.awan@akhuwat.org.pk>
2023-12-27 18:48:39 +0500

    commit chngs

--- Common/Users/Entities/ProfessionalProfileWorkExperiences.php
+++ Common/Users/Entities/ProfessionalProfileWorkExperiences.php
@@ -12,7 +12,7 @@ class ProfessionalProfileWorkExperiences extends Model
     protected $fillable = ['user_id', 'professional_profile_id','job_title','company_name','company_website','start_date','end_date','work_email','work_type','location','country_id','state_id','city_id','remote_work','primary_role','job_duties','skills','tools','project_description','workplace_name'];
 
     protected $table='professional_profile_work_experiences';
-    
+    protected $with=['country_name', 'city_name'];
     protected static function newFactory()
     {
         return \Common\Users\Database\factories\ProfessionalProfileWorkExperiencesFactory::new();
@@ -26,5 +26,15 @@ class ProfessionalProfileWorkExperiences extends Model
     public function getToolsAttribute($value)
     {
         return json_decode($value);
+    }
+
+    public function getCityNameAttribute()
+    {
+       return CityName($this->city_id);
+    }
+
+    public function getCountryNameAttribute()
+    {
+       return CountryName($this->country_id);
     }
 }
