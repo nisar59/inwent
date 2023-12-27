9d3cedebb189a57717b90a1c40ac26054931b645
Mahar Nisar <umair.awan@akhuwat.org.pk>
2023-12-27 18:49:48 +0500

    commit changes

--- Common/Users/Entities/ProfessionalProfileWorkExperiences.php
+++ Common/Users/Entities/ProfessionalProfileWorkExperiences.php
@@ -12,7 +12,7 @@ class ProfessionalProfileWorkExperiences extends Model
     protected $fillable = ['user_id', 'professional_profile_id','job_title','company_name','company_website','start_date','end_date','work_email','work_type','location','country_id','state_id','city_id','remote_work','primary_role','job_duties','skills','tools','project_description','workplace_name'];
 
     protected $table='professional_profile_work_experiences';
-    protected $with=['country_name', 'city_name'];
+    protected $appends=['country_name', 'city_name'];
     protected static function newFactory()
     {
         return \Common\Users\Database\factories\ProfessionalProfileWorkExperiencesFactory::new();
