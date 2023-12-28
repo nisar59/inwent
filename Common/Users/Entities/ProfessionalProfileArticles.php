c1de27877c0ff268000f316f489dbcfcbd6a27aa
Mahar Nisar <umair.awan@akhuwat.org.pk>
2023-12-28 12:27:54 +0500

    commmit changes

--- Common/Users/Entities/ProfessionalProfileArticles.php
+++ Common/Users/Entities/ProfessionalProfileArticles.php
@@ -11,9 +11,19 @@ class ProfessionalProfileArticles extends Model
 
     protected $fillable = ['user_id', 'professional_profile_id','article_title','event_organizer','article_abstract','article_cover_image','article_link','article_tags','article_tag_line','workplace_name','country_id','city_id'];
     protected $table='professional_profile_articles';
-    
+    protected $appends=['country_name', 'city_name'];
     protected static function newFactory()
     {
         return \Common\Users\Database\factories\ProfessionalProfileArticlesFactory::new();
+    }
+
+    public function getCityNameAttribute()
+    {
+       return CityName($this->city_id);
+    }
+
+    public function getCountryNameAttribute()
+    {
+       return CountryName($this->country_id);
     }
 }
