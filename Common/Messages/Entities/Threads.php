0a7c4c117cb2559f2570613ef544568d187a7c26
Mahar Nisar <umair.awan@akhuwat.org.pk>
2023-12-31 18:10:41 +0500

    commit changes

--- Common/Messages/Entities/Threads.php
+++ Common/Messages/Entities/Threads.php
@@ -5,13 +5,14 @@ namespace Common\Messages\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\User;
+use Common\Messages\Entities\Messages;
 class Threads extends Model
 {
     use HasFactory;
 
     protected $table='chat_threads';
     protected $fillable = ['sender_id', 'receiver_id', 'module'];
-    protected $with=['sender', 'receiver'];
+    protected $with=['sender', 'receiver', 'last_message'];
     protected static function newFactory()
     {
         return \Common\Messages\Database\factories\ThreadsFactory::new();
@@ -26,5 +27,11 @@ class Threads extends Model
     {
         return $this->hasOne(User::class, 'id', 'receiver_id');
     }
+
+    public function last_message()
+    {
+        return $this->hasOne(Messages::class, 'thread_id', 'id')->latest();
+    }
+
 
 }
