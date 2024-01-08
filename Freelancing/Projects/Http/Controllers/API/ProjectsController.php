3a6ce64e7006f82485749cd27a13d23b16606085
Mahar Nisar <umair.awan@akhuwat.org.pk>
2024-01-08 21:24:54 +0500

    comt

--- Freelancing/Projects/Http/Controllers/API/ProjectsController.php
+++ Freelancing/Projects/Http/Controllers/API/ProjectsController.php
@@ -180,8 +180,8 @@ class ProjectsController extends Controller
             $projects=Projects::whereNot('user_id', $user_id);
 
             if($req->keyword!=null){
-                $projects->where('job_title', 'ilike', '%'.$req->keyword.'%')
-                ->Orwhere('project_name', 'ilike', '%'.$req->keyword.'%');
+                $projects->where('LCASE(job_title)', 'like', '%'.strtolower($req->keyword).'%')
+                ->Orwhere('LCASE(project_name)', 'like', '%'.strtolower($req->keyword).'%');
             }
 
             $total = $projects->count();
